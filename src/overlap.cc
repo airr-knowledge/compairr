c1e15c0d66f1b3b42ff2c9b2481c2ff4e30e10f6
Sam Wollenburg <Samuel.Wollenburg@UTSouthwestern.edu>
2026-02-13 21:14:12 +0000

    remove header when using -p and -q together

--- src/overlap.cc
+++ src/overlap.cc
@@ -994,24 +994,28 @@ void overlap(db * d1_local, db * d2_local, bool d2eqd1)
                 "#repertoire_id_1\tsequence_id_1\t"
                 "duplicate_count_1\tv_call_1\tj_call_1\t%s_1",
                 seq_header);
-      } else {
-        fprintf(pairsfile,
-                "sequence_id_1\t",
-                seq_header);
-      }
+      } 
+      // else {
+      //   fprintf(pairsfile,
+      //           "sequence_id_1\t",
+      //           seq_header);
+      // }
 
       for (int k = 0; k < keep_columns_count; k++)
         fprintf(pairsfile, "\t%s_1", keep_columns_names[k]);
+
       if (!opt_seq_id_only) {
         fprintf(pairsfile,
                 "\trepertoire_id_2\tsequence_id_2\t"
                 "duplicate_count_2\tv_call_2\tj_call_2\t%s_2",
                 seq_header);
-      } else {
-        fprintf(pairsfile,
-                "sequence_id_2\t",
-                seq_header);
-      }
+      } 
+      // else {
+      //   fprintf(pairsfile,
+      //           "sequence_id_2\t",
+      //           seq_header);
+      // }
+      
       for (int k = 0; k < keep_columns_count; k++)
         fprintf(pairsfile, "\t%s_2", keep_columns_names[k]);
       if (opt_distance)
